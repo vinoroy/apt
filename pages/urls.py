d2a65050afec61e8c26de11d132828332793145d
Vincent Roy <vinoroy70@gmailcom>
2020-01-22 05:56:33 -0500

    version 20/01/22

--- pages/urls.py
+++ pages/urls.py
@@ -9,8 +9,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('pages/pages/<str:name>', views.pages, name='pages'),
-    path('pages/list/',views.list, name='list'),
-    path('pages/detail/<int:id>',views.detail, name='detail'),
-    path('pages/propos/',views.propos, name='propos'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,11 +5,14 @@ from . models import Apartment, Property, Page,Size ,Sector
 
 from .forms import SizeForm, SectorForm
 
+from django.core.mail import send_mail
+from django.conf import settings
+
+
 def index(request):
-    #return HttpResponse("<h1>Bienvenue chez Logi 2001</h1>")
 
     task = None
-    submitted = False
+    submitted = ''
 
     tabs = Page.objects.all()
 
@@ -26,36 +29,60 @@ def index(request):
 
     if request.method == 'POST':
 
-        print('post received')
 
+        # get the post request object
         p = request.POST
 
-        print(p)
+        if 'SEARCH' in p:
 
-        print(p.getlist('sizeSel'))
-
-        sizeList = p.getlist('sizeSel')
-
-        sectorList = p.getlist('sectorSel')
-
-
-        if not sizeList:
-            aptFilter = Apartment.objects.filter(property__sector__sector__in=sectorList)
-
-        elif not sectorList:
-            aptFilter = Apartment.objects.filter(size__size__in=sizeList)
-
-        else:
-            aptFilter = Apartment.objects.filter(size__size__in=sizeList).filter(property__sector__sector__in=sectorList)
+            # section to print post request results for degugging
+            print(p)
+            #print(p.getlist('sizeSel'))
+            #print(p.get('name'))
 
 
-        submitted = True
+            # get the search values for size and sector
+            sizeList = p.getlist('sizeSel')
+            sectorList = p.getlist('sectorSel')
+
+
+            # execute the query based on the search values
+            if not sizeList:
+                aptFilter = Apartment.objects.filter(property__sector__sector__in=sectorList)
+
+            elif not sectorList:
+                aptFilter = Apartment.objects.filter(size__size__in=sizeList)
+
+            else:
+                aptFilter = Apartment.objects.filter(size__size__in=sizeList).filter(property__sector__sector__in=sectorList)
+
+            # set the submitted value
+            submitted = 'Queries'
+
+
+        elif 'MESSAGE' in p:
+
+            # get the values for the contact form
+            name = p.get('name')
+            email = p.get('email')
+            comments = p.get('comments')
+
+            # send the email
+            subject = 'Demande d`information de : ' + name + ' à : ' + email
+            email_from = settings.EMAIL_HOST_USER
+            recipient_list = ['logis2001info@gmail.com', ]
+            send_mail(subject, comments, email_from, recipient_list)
+
+            # set the submitted value to true
+            submitted = 'Message'
 
 
 
     else:
 
         pass
+
+
 
 
     context = {'tabs': tabs, 'aptFilter': aptFilter, 'propList': propList, 'sizes': sizes, 'sectors': sectors, 'submitted': submitted}
@@ -65,42 +92,4 @@ def index(request):
 
 
 
-def pages(request,name):
-
-    pagename = '/' + name
-    pg = Page.objects.get(permalink=pagename)
-    context = {
-        'title': pg.title,
-        'content': pg.bodytext,
-        'last_updated': pg.update_date,
-        }
-
-    return render(request, 'pages/pages.html', context)
-
-
-
-def list(request):
-
-    aptList = Apartment.objects.all()
-    propList = Property.objects.all()
-
-    context = {'aptList': aptList,'propList':propList}
-
-    return render(request, 'pages/list.html',context)
-
-
-def detail(request,id):
-
-    #apt = Apartment.objects.get(id=id)
-
-    apt = Apartment.objects.get(pk=id)
-
-    context = {'apt': apt}
-
-    return render(request, 'pages/detail.html', context)
-
-
-def propos(request):
-
-    return render(request, "pages/propos.html")
 
